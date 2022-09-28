91905a8dd7a1245e0e0ad87a1ec89b64271503bb
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2022-09-28 10:30:57 +0200

    exo 4 debut

--- src/main/java/exo4/Fenetre.java
+++ src/main/java/exo4/Fenetre.java
@@ -2,10 +2,12 @@ package exo4;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Fenetre extends JFrame {
+public class Fenetre extends JFrame implements ActionListener {
 
     public Fenetre(int nbButtons) {
         super("Une fenêtre"); // ou setTitle("...");
@@ -17,31 +19,49 @@ public class Fenetre extends JFrame {
         for (int i = 0; i < nbButtons; i++) {
             JButton button = new JButton("Button " + (i+1));
             contentPane.add(button);
+            button.addActionListener(this);
         }
 
-        this.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                System.out.println("agtaegzagzgzagze");
-            }
+        ;
 
-            @Override
-            public void keyPressed(KeyEvent e) {
-                try{
-                    System.out.println("Key pressed code=" + e.getKeyCode() + ", char=" + e.getKeyChar());
-                }catch (Exception exception){
-                    System.out.println("la");
-                }
-
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                System.out.println("hhfhfdhfdfh");
-            }
-        });
+//        this.addKeyListener(new KeyListener() {
+//            @Override
+//            public void keyTyped(KeyEvent e) {
+//                System.out.println("agtaegzagzgzagze");
+//            }
+//
+//            @Override
+//            public void keyPressed(KeyEvent e) {
+//                try{
+//                    System.out.println("Key pressed code=" + e.getKeyCode() + ", char=" + e.getKeyChar());
+//                }catch (Exception exception){
+//                    System.out.println("la");
+//                }
+//
+//            }
+//
+//            @Override
+//            public void keyReleased(KeyEvent e) {
+//                System.out.println("hhfhfdhfdfh");
+//            }
+//        });
 
 
         setVisible(true); // ou show(), // affiche la fenêtre
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        System.out.println("lala");
+        Object source = e.getSource();
+        System.out.println(source);
+//        if (source == b1) { //Suppose que l'on est dans la portée //de b1 qui contient une référence au // bouton // action liée au bouton b1
+//        }
+//        else if (source == b2) { // action liée au bouton b2
+//        }
+//        else if (source == f) { // action liée au JTextField f
+//        }
+
     }
 }
