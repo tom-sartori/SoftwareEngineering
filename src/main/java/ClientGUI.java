2ed5cbab43308833fa2b759bacba643fb682206c
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2022-10-27 09:36:24 +0200

    added swing interfaces to the project

---
+++ src/main/java/ClientGUI.java
@@ -0,0 +1,176 @@
+import client.ChatClient;
+import common.ChatIF;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.awt.*;
+import java.io.*;
+import java.util.Objects;
+
+import client.ChatClient;
+import common.ChatIF;
+
+import javax.swing.*;
+
+import static java.lang.Thread.sleep;
+
+// This file contains material supporting section 3.7 of the textbook:
+// "Object-Oriented Software Engineering" and is issued under the open-source
+// license found at www.lloseng.com
+
+/**
+ * This class constructs the UI for a chat client.  It implements the
+ * chat interface in order to activate the display() method.
+ * Warning: Some the code here is cloned in ServerConsole
+ *
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Dr Timothy C. Lethbridge
+ * @author Dr Robert Lagani&egrave;re
+ * @version July 2000
+ */
+public class ClientGUI extends JFrame implements ChatIF {
+        //Class variables *************************************************
+
+        /**
+         * The default port to connect on.
+         */
+        final public static int DEFAULT_PORT = 5555;
+
+        //Instance variables **********************************************
+
+        /**
+         * The instance of the client that created this ConsoleChat.
+         */
+        ChatClient client;
+
+        private JTextArea jTextArea;
+
+        //Constructors ****************************************************
+
+        /**
+         * Constructs an instance of the ClientConsole UI.
+         *
+         * @param host The host to connect to.
+         * @param port The port to connect on.
+         */
+        public ClientGUI(String host, int port) {
+            super("SimpleChat 4"); // ou setTitle("...");
+            try {
+                client = new ChatClient(host, port, this);
+            }
+            catch(IOException exception) {
+                System.out.println("Error: Can't setup connection!" + " Terminating client.");
+                System.exit(1);
+            }
+
+
+            setSize(1920, 1080); // ou setBounds(...);
+
+            Container contentPane = getContentPane();
+
+            /**
+             *  NORTH PART LABEL "Welcome to simpleChat 4"
+             */
+            JPanel topLabel = new JPanel();
+
+            JLabel topMessage = new JLabel("Welcome to simpleChat 4 !! :)");
+            topLabel.add(topMessage);
+
+            contentPane.add(topLabel, BorderLayout.NORTH);
+
+            /**
+             *  CENTER PART TEXT AREA WHERE DATA WILL BE WRITTEN WITH SERVER RESPONSES
+             */
+
+            this.jTextArea = new JTextArea();
+            contentPane.add(jTextArea, BorderLayout.CENTER);  // Text area middle.
+
+            /**
+             *  SOUTH PANE WITH TEXTAREA , SEND BUTTON AND QUIT BUTTON
+             */
+
+            JPanel southPanel = new JPanel();
+            southPanel.setLayout(new GridLayout(3,1));
+
+            JPanel sendMessagesPanel = new JPanel();
+            sendMessagesPanel.setLayout(new GridLayout(1,2));
+
+            JTextArea sendMessageTextArea = new JTextArea();
+
+            JButton sendButton = new JButton("Send");
+            sendButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(sendMessageTextArea.getText().equals("#disco")){
+                    }
+                    else{
+                        client.handleMessageFromClientUI(sendMessageTextArea.getText());
+                        sendMessageTextArea.setText("");
+                    }
+                }
+            });
+
+            JLabel writeMessageLabel = new JLabel("Write your message below :");
+            southPanel.add(writeMessageLabel);
+
+            sendMessagesPanel.add(sendMessageTextArea);
+            sendMessagesPanel.add(sendButton);
+
+
+            southPanel.add(sendMessagesPanel);
+
+            JButton quitButton = new JButton("Quit");
+            quitButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    client.quit();
+                }
+            });
+
+            southPanel.add(quitButton);
+
+            contentPane.add(southPanel, BorderLayout.SOUTH);
+
+            setVisible(true); // ou show(), // affiche la fenêtre
+        }
+
+
+        //Instance methods ************************************************
+
+        /**
+         * This method overrides the method in the ChatIF interface.  It
+         * displays a message onto the screen.
+         *
+         * @param message The string to be displayed.
+         */
+        public void display(String message){
+            this.jTextArea.append(message + "\n");
+        }
+
+        //Class methods ***************************************************
+
+        /**
+         * This method is responsible for the creation of the Client UI.
+         *
+         * @param args [0] The host to connect to.
+         */
+        public static void main(String[] args) {
+
+            String host;
+            int port = 0;  //The port number
+
+            try {
+                host = args[0];
+            }
+            catch(ArrayIndexOutOfBoundsException e) {
+                host = "localhost";
+            }
+            ClientGUI chat = new ClientGUI(host, DEFAULT_PORT);
+        }
+
+    }
+
---
+++ src/main/java/ServerGUI.java
@@ -0,0 +1,120 @@
+import common.ChatIF;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ServerGUI extends JFrame implements ChatIF {
+
+    EchoServer echoServer;
+    private JTextArea jTextArea;
+
+    public ServerGUI() {
+        super("SimpleChat 4 | Server"); // ou setTitle("...");
+
+        this.echoServer = new EchoServer(5555, this);
+
+
+        setSize(1920, 1080); // ou setBounds(...);
+
+        Container contentPane = getContentPane();
+
+        /**
+         *  NORTH PART LABEL "Welcome to simpleChat 4"
+         */
+        JPanel topLabel = new JPanel();
+
+        JLabel topMessage = new JLabel("Welcome to simpleChat 4 !! :)");
+        topLabel.add(topMessage);
+
+        contentPane.add(topLabel, BorderLayout.NORTH);
+
+        /**
+         *  CENTER PART TEXT AREA WHERE DATA WILL BE WRITTEN WITH SERVER RESPONSES
+         */
+
+        this.jTextArea = new JTextArea();
+        contentPane.add(jTextArea, BorderLayout.CENTER);  // Text area middle.
+
+        /**
+         *  SOUTH PANE WITH TEXTAREA , SEND BUTTON AND QUIT BUTTON
+         */
+
+        JPanel southPanel = new JPanel();
+        southPanel.setLayout(new GridLayout(3,1));
+
+        JPanel sendMessagesPanel = new JPanel();
+        sendMessagesPanel.setLayout(new GridLayout(1,2));
+
+        JTextArea sendMessageTextArea = new JTextArea();
+
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                echoServer.handleMessageFromServerUI(sendMessageTextArea.getText());
+                sendMessageTextArea.setText("");
+            }
+        });
+
+        JLabel writeMessageLabel = new JLabel("Write your message below :");
+        southPanel.add(writeMessageLabel);
+
+        sendMessagesPanel.add(sendMessageTextArea);
+        sendMessagesPanel.add(sendButton);
+
+
+        southPanel.add(sendMessagesPanel);
+
+        JButton quitButton = new JButton("Quit");
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                echoServer.handleMessageFromServerUI("#close");
+                System.exit(0);
+            }
+        });
+
+        southPanel.add(quitButton);
+
+        contentPane.add(southPanel, BorderLayout.SOUTH);
+
+        setVisible(true); // ou show(), // affiche la fenêtre
+    }
+
+    public void display(String msg){
+        this.jTextArea.append(msg + "\n");
+    }
+
+    public EchoServer getEchoServer() {
+        return echoServer;
+    }
+
+
+    /**
+     * This method is responsible for the creation of
+     * the server instance (there is no UI in this phase).
+     *
+     * @param args [0] The port number to listen on.  Defaults to 5555
+     *          if no argument is entered.
+     */
+    public static void main(String[] args)
+    {
+        int port = 0; //Port to listen on
+
+        try
+        {
+            port = Integer.parseInt(args[0]); //Get port from command line
+        }
+        catch(Throwable t)
+        {
+            port = EchoServer.DEFAULT_PORT; //Set port to 5555
+        }
+
+        ServerGUI serverConsole = new ServerGUI();
+    }
+}
+
